ac5b74fdb55bb7350fde606384ef174f7454fed4
joaogabrielvalenca <joaogabrielvalenca@gmail.com>
2023-06-20 16:03:11 -0300

    teste req01

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
-console.log(app);
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+
+app.get('/talker', (_request, response) => {
+  try {
+      const data = fs.readFile('./talker.json', 'utf-8');
+      const talkers = JSON.parse(data);
+      response.status(HTTP_OK_STATUS).json(talkers);
+    } catch (parseError) {
+      console.error(parseError);
+      response.status(404).json([]);
+    }
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
